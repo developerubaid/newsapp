d8d2f30be80ea287f9f9fa1d4e5830b3649344bc
Hafiz Ubaidullah Anwar <developerubaid@gmail.com>
2021-10-31 07:57:32 +0500

    BSv5 | Navbar | News | NewsItem | Next & Prev BTN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
 
 export default class App extends Component {
   name = "Ubaidullah";
   render() {
-    return <div>This is a class base component {this.name}</div>;
+    return (
+      <div>
+        <Navbar />
+        <News />
+      </div>
+    );
   }
 }
---
+++ src/Components/News.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    // console.log("Compontent Did Mount");
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=22bae92c74fe4c3d833b36bb970f1678&page=1";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    // console.log(parsedData);
+
+    this.setState({ articles: parsedData.articles });
+  }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=22bae92c74fe4c3d833b36bb970f1678&page=${
+      this.state.page - 1
+    }`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=22bae92c74fe4c3d833b36bb970f1678&page=${
+      this.state.page + 1
+    }`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+    });
+  };
+  render() {
+    return (
+      <div className="container my-4">
+        <h1>City News - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4 my-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imgUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://toppng.com/uploads/preview/headline-news-11551056985ectz5movvq.png"
+                  }
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="contianer d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-primary"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
