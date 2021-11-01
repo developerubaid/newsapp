94f208c82f52f0ac0aba78b49c333ea7618f5c5f
ubaidullahanwar <ubaidullahanwar3@gmail.com>
2021-11-01 23:44:15 +0500

    Navbar Update
    News Categories added to the Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,7 +25,7 @@ export class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className="nav-link " aria-current="page" href="/">
                   Home
                 </a>
               </li>
@@ -34,6 +34,21 @@ export class Navbar extends Component {
                   About
                 </a>
               </li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">business</a></li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">entertainment</a></li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">general</a></li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">health</a></li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">science</a></li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">sports</a></li>
+               <li className="nav-item">
+                <a className="nav-link" href="/about">technology</a></li>
+
             </ul>
           </div>
         </div>
